8432f6f932dba01906293ee1a878ea2275f91e0c
Rafik HARZI <7rouz@users.noreply.github.com>
2020-05-14 11:58:16 +0200

    remove non-priting characters from file
    add sed command that removes non printing characters from file

--- remove_spaces_from_var.sh
+++ remove_spaces_from_var.sh
@@ -12,3 +12,6 @@ res=$(echo "$x" | sed 's/^[ \t]*//')
 # remove trailing spaces/tabs
 x=" value  "
 res=$(echo "$x" | sed 's/[ \t]*$//')
+
+# remove non printing characters from a file
+sed -i 's/[^[:print:]]//g' file
